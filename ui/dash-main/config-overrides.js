c712ff392c071f7738db10bd16ac70976fb4e872
Stefan Marx <smarx@marxworld.net>
2018-07-06 16:28:46 +0200

    Cleanup

--- ui/dash-main/config-overrides.js
+++ ui/dash-main/config-overrides.js
@@ -23,7 +23,8 @@ module.exports = function override(config, env) {
   };
 
      config = rewireLess.withLoaderOptions({
-        modifyVars: {  },
+      javascriptEnabled: true, 
+        modifyVars: {  }
        })(config, env);
   return config;
 };
--- ui/dash-main/src/index.tsx
+++ ui/dash-main/src/index.tsx
@@ -1,17 +1,20 @@
-import {LogService} from "databoard-core-ui";
+import * as UIServices from 'databoard-core-ui';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { LogLevel } from "../node_modules/databoard-core-ui/dist/util/logging/LogService";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './style/index.less';
+
+
+
+
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
 
-const  s = new LogService("Master");
-s.log(LogLevel.Info,"STARTED");
+const  s = new UIServices.LogService("Master");
+s.log(s.Level,"STARTED");
 
 registerServiceWorker();
